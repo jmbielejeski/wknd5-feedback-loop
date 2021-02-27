79fc7607be0d14e748d51b8eacde4b1bca6a0c53
jmbielejeski <jmbielejeski@gmail.com>
2021-02-27 12:38:11 -0700

    fleshing out feeling component

--- src/components/HowAreYouFeeling/HowAreYouFeeling.jsx
+++ src/components/HowAreYouFeeling/HowAreYouFeeling.jsx
@@ -1,6 +1,21 @@
+import { Link } from 'react-router-dom';
+
 function HowAreYouFeeling() {
   return (
-    <h1>How are you feeling today?</h1>
+    <>
+      <h1>How are you feeling today?</h1>
+      <form>
+        <label for="dropdown">Feeling?</label>
+        <select id="dropdown">
+          <option value ='1'>1</option>
+          <option value ='2'>2</option>
+          <option value ='3'>3</option>
+          <option value ='4'>4</option>
+          <option value ='5'>5</option>
+        </select>
+        <Link to="/understanding">Next</Link>
+      </form>
+    </>
   )
 }
 
