e8cc6143690acfaec93db9e861fcf5904bb553d0
jmbielejeski <jmbielejeski@gmail.com>
2021-02-27 13:36:08 -0700

    setup feeling component and understanding component

--- src/components/HowAreYouFeeling/HowAreYouFeeling.jsx
+++ src/components/HowAreYouFeeling/HowAreYouFeeling.jsx
@@ -1,22 +1,43 @@
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 function HowAreYouFeeling() {
+
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+// handle submit to send the chosen option value to Redux to store in state
+const handleSubmit = (event) => {
+  event.preventDefault();
+
+  let feelingValue = document.getElementById("feelingInput").value;
+
+  //dispatch chosen value to redux 
+  dispatch({
+    type: 'SET_FEELING_FEEDBACK',
+    payload: feelingValue
+  })
+  history.push('/understanding')
+}
+
   return (
     <>
       <h1>How are you feeling today?</h1>
-      <form>
-        <label for="dropdown">Feeling?</label>
-        <select id="dropdown">
-          <option value ='1'>1</option>
-          <option value ='2'>2</option>
-          <option value ='3'>3</option>
-          <option value ='4'>4</option>
-          <option value ='5'>5</option>
-        </select>
-        <Link to="/understanding">Next</Link>
-      </form>
+        <label for="feelingInput">Feeling?</label>
+          <select required name="feelingInput" id="feelingInput">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+            <option value="5">5</option>
+          </select>
+        
+        <button onClick={handleSubmit}>Next</button>
+
     </>
   )
 }
 
 export default HowAreYouFeeling;
+
+{/*  */}
